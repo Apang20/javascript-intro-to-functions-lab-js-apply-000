25e793b08f1f23ae0eb7ff4a099231e2f103fc08
convivial-parser-7775 <avelonpang@gmail.com>
2020-08-31 17:45:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-console.log(shout);
-console.log(whisper);
+console.log(shout(string));
+console.log(whisper(string));
 
 function sayHiToGrandma(string) {
 if (string === "I love you, Grandma.") {
-  return "I love you, too";
+  return "I love you, too.";
 }
 if (string === string.toUpperCase()) {
   return "YES INDEED!";
