68d2ea70d89d75245e6addb9cd874f8313265e7d
convivial-parser-7775 <avelonpang@gmail.com>
2020-08-31 17:42:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ function whisper(string) {
 console.log(string)
 
 function sayHiToGrandma(string) {
+if (string === "I love you, Grandma.") {
+  return "I love you too";
+}
 if (string === string.toUpperCase()) {
   return "YES INDEED";
-} else if ()
+} else {
+  return "I can't hear you!";
+}
 }
