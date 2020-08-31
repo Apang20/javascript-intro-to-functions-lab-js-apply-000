73695a00f004d3d0544f73a055c30b6ea46425f6
convivial-parser-7775 <avelonpang@gmail.com>
2020-08-31 17:40:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+console.log(string)
+
+function sayHiToGrandma(string) {
+if (string === string.toUpperCase()) {
+  return "YES INDEED";
+} else if ()
+}
